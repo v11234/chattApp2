ef13c56e089ade491442dd94fb5d5da4f41ea666
v11234 <vanettijunior@gmail.com>
2025-10-12 11:30:26 +0100

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { signup, login,logout, updateProfilepic} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router=express.Router();
-
+router.use(arcjetProtection);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout",logout);
