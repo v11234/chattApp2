2833ef9854e7f5b7a30fc0efb83e24af3b021ef8
v11234 <vanettijunior@gmail.com>
2025-10-14 14:01:08 +0100

    frontend-setup

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div className='flex w-full '>
+        <div className=' flex align-center justify-center items-center '>
+        <h1 className='text-blue-600 text-2xl '>Bienvenue un fois de plus</h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,11 @@
+import {create} from  "zustand"
+
+export const useAuthStore= create((set)=>({
+authUser:null,
+isLoggedIn:false,
+
+
+login:()=>{
+console.log("login successfully");
+}
+}))
